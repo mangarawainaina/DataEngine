94d78b951fb367d0fbee9b5eb6d74349d86e3afb
alchemist_primus <85114274+AlchemistPrimus@users.noreply.github.com>
2021-12-14 22:48:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from flask import Flask
 from views import routes
 from routes import view
 
-
-def create_app():
     
-    app=Flask(__name__)
+app=Flask(__name__)
     
-    app.register_blueprint(routes)
-    app.register_blueprint(view)
+app.register_blueprint(routes)
+app.register_blueprint(view)
     
-    return app.run(debug=True)
+    
